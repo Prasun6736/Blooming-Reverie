2a3ad643989418705def44e5904aebb7d5ffdb93
Prasun Mridha <prasunmridha6@gmail.com>
2024-04-26 20:34:58 +0530

    'ok'

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
+import Container from "./components/Container";
+import HomeBanner from "./components/HomeBanner";
+
 export default function Home() {
-  return <div></div>;
+  return( <div className="p-8">
+    <Container>
+      <div>
+        <HomeBanner />
+      </div>
+    </Container>
+  </div>
+  );
   
 }
