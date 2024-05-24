b41964f6c81d7c8f84f774e4c9dc393e5a0633b9
Prasun Mridha <prasunmridha6@gmail.com>
2024-05-24 12:59:38 +0530

    Your commit message

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+import { products } from "@/utils/products";
 import Container from "./components/Container";
 import HomeBanner from "./components/HomeBanner";
+import { truncateText } from "@/utils/truncateText";
 
 export default function Home() {
   return( <div className="p-8">
     <Container>
       <div>
         <HomeBanner />
+      </div>
+      <div>
+        {products.map((product: any) => {
+          return <div>{truncateText (product.name)}</
+          div>;
+        })}
       </div>
     </Container>
   </div>
